f8bc6be8b0fb19a0baa10080cf731ea427774ae8
Alexandre Froger <alex@froger.me>
2018-08-25 02:14:15 +0800

    Update class-wp-update-migrate.php

--- class-wp-update-migrate.php
+++ class-wp-update-migrate.php
@@ -4,7 +4,7 @@
  * WordPress plugins and themes update path library.
  *
  * @author Alexandre Froger
- * @version 1.0.0
+ * @version 1.1.0
  * @see https://github.com/froger-me/wp-update-migrate
  * @copyright Alexandre Froger - https://www.froger.me
  */
@@ -14,26 +14,32 @@
 /*================================================================================================ */
 
 /**
- * Copy/paste this section to your main plugin file and uncomment the code below to enable migrations
+ * Copy/paste this section to your main plugin file or theme functions.php and selectively uncomment the
+ * code below to enable migrations
  *
  * WARNING - READ FIRST:
- * Before enabling migrations, setup your plugin properly by making sure:
- * - the wp-update-migrate folder library is present in a lib directory at the root of the plugin
+ * Before enabling migrations, setup your plugin or theme properly by making sure:
+ * - the wp-update-migrate folder library is present in a lib directory at the root of the plugin or theme
  * - to change $example_update_migrate variable name
- * - to change 'example_prefix' with a unique prefix identifier for your plugin, snake_case format
+ * - to change 'example_prefix' with a unique prefix identifier for your plugin or theme, snake_case format
  * - to change 'example_function' with the name of the function to remove from or add to the action queue
- * - to adjust the action priority in the remove_action call
- * - to have an 'updates' directory at the root of the plugin
+ * - to have an 'updates' directory at the root of the plugin or theme
  * - to name each file in the 'updates' folder with a version number as file name (example: 1.5.3.php)
  * - each update file in the 'updates' directory have a single update function, and do not include any logic outside of that function
  * - the update function name in each update file follows the pattern: [example_prefix]_update_to_[version]
  *     - example: in 1.5.3.php, the function is my_plugin_update_to_1_5_3 with [example_prefix] = my_plugin
+ *     - example: in 2.4.9.php, the function is my_theme_update_to_2_4_9 with [example_prefix] = my_theme
  * - each update function returns (bool) true in case of success, a WP_Error object otherwise
  **/
 
+/** For plugins **/
 // 	require_once plugin_dir_path( __FILE__ ) . 'lib/wp-update-migrate/class-wp-update-migrate.php';
-
 // $example_update_migrate = WP_Update_Migrate::get_instance( __FILE__, 'example_prefix' );
+
+/** For themes **/
+// require_once  trailingslashit( get_stylesheet_directory() ) . 'lib/wp-update-migrate/class-wp-update-migrate.php';
+// $example_update_migrate = WP_Update_Migrate::get_instance( get_stylesheet(), 'example_prefix' );
+
 
 // if ( false === $example_update_migrate->get_result() ) {
 	///**
@@ -71,47 +77,79 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 
 		protected $failed_update_info;
 		protected $success_update_info;
-		protected $plugin_name;
-		protected $plugin_prefix;
-		protected $plugin_dir;
+		protected $package_name;
+		protected $package_prefix;
+		protected $package_dir;
 		protected $to_version;
 		protected $from_version;
 		protected $update_result;
+		protected $package_type;
 
-		private function __construct( $plugin_file, $plugin_prefix ) {
+		private function __construct( $package_handle, $package_prefix ) {
 
 			if ( ! wp_doing_ajax() ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+				require_once ABSPATH . 'wp-admin/includes/file.php';
 
-				$plugin_data              = get_plugin_data( $plugin_file );
-				$current_recorded_version = get_option( $plugin_prefix . '_plugin_version' );
-				$latest_version           = $plugin_data['Version'];
-				$is_version_current       = version_compare( $current_recorded_version, $latest_version, '>=' );
+				$this->init_package_type( $package_handle );
 
-				if ( ! $is_version_current ) {
-					$this->plugin_name   = $plugin_data['Name'];
-					$this->plugin_dir    = plugin_dir_path( $plugin_file );
-					$this->plugin_prefix = $plugin_prefix;
-					$this->from_version  = $current_recorded_version;
-					$this->to_version    = $latest_version;
+				if ( 'plugin' === $this->package_type ) {
+					require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
-					add_action( 'init', array( $this, 'init' ), -10, 0 );
-					add_action( 'plugins_loaded', array( $this, 'update' ), -10, 0 );
+					$update_hook        = 'plugins_loaded';
+					$plugin_data        = get_plugin_data( $package_handle );
+					$latest_version     = $plugin_data['Version'];
+					$this->package_name = $plugin_data['Name'];
+					$this->package_dir  = plugin_dir_path( $package_handle );
+				} elseif ( 'theme' === $this->package_type ) {
+					require_once ABSPATH . 'wp-includes/theme.php';
+
+					$update_hook        = 'after_setup_theme';
+					$theme_info         = wp_get_theme( $package_handle );
+					$latest_version     = $theme_info->get( 'Version' );
+					$this->package_name = $theme_info->get( 'Name' );
+					$this->package_dir  = $theme_info->get_stylesheet_directory();
+				}
+
+				if ( ! is_null( $this->package_type ) ) {
+					$current_recorded_version = get_option( $package_prefix . '_' . $this->package_type . '_version' );
+					$is_version_current       = version_compare( $current_recorded_version, $latest_version, '>=' );
+
+					if ( ! $is_version_current ) {
+						$this->package_prefix = $package_prefix;
+						$this->from_version   = $current_recorded_version;
+						$this->to_version     = $latest_version;
+
+						add_action( 'init', array( $this, 'init' ), -10, 0 );
+						add_action( $update_hook, array( $this, 'update' ), -10, 0 );
+					}
 				}
 			}
 
-			wp_cache_set( $plugin_prefix, $this, 'wp-update-migrate' );
+			wp_cache_set( $package_prefix, $this, 'wp-update-migrate' );
 		}
 
-		public static function get_instance( $plugin_file, $plugin_prefix ) {
-			$cache = wp_cache_get( $plugin_prefix, 'wp-update-migrate' );
+		public static function get_instance( $package_handle, $package_prefix ) {
+			$cache = wp_cache_get( $package_prefix, 'wp-update-migrate' );
 
 			if ( ! $cache ) {
 
-				return new self( $plugin_file, $plugin_prefix );
+				return new self( $package_handle, $package_prefix );
 			}
 
 			return $cache;
+		}
+
+		public function init_package_type( $package_handle ) {
+
+			if ( false !== strpos( $package_handle, self::get_content_dir() . 'plugins' ) ) {
+				$this->package_type = 'plugin';
+			} else {
+				$theme_obj = wp_get_theme( $package_handle );
+
+				if ( $theme_obj->exists() ) {
+					$this->package_type = 'theme';
+				}
+			}
 		}
 
 		public function get_result() {
@@ -123,26 +161,30 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 			$class   = 'notice notice-error is-dismissible';
 			$message = '<p>' . $this->failed_update_info . '</p>';
 
-			$message .= '<p>' . __( 'The plugin will not have any effect until the issues are resolved.', 'wp-update-migrate' ) . '</p>';
+			// translators: %1$s is the package type
+			$message .= '<p>' . sprintf( __( 'The %1$s will not have any effect until the issues are resolved.', 'wp-update-migrate' ), $this->package_type ) . '</p>';
 
 			printf( '<div class="%1$s">%2$s</div>', $class, $message ); // @codingStandardsIgnoreLine
 		}
 
 		public function update_success_notice() {
 			$class = 'notice notice-success is-dismissible';
-			// translators: %1$s is the plugin version to update to
-			$title   = $this->plugin_name . ' - ' . sprintf( __( 'Success updating to version %1$s', 'wp-update-migrate' ), $this->to_version );
+			// translators: %1$s is the package version to update to
+			$title   = $this->package_name . ' - ' . sprintf( __( 'Success updating to version %1$s', 'wp-update-migrate' ), $this->to_version );
 			$message = '<h3>' . $title . '</h3><p>' . $this->success_update_info . '</p>';
 
 			printf( '<div class="%1$s">%2$s</div>', $class, $message ); // @codingStandardsIgnoreLine
 		}
 
 		public function init() {
-			load_plugin_textdomain(
-				'wp-update-migrate',
-				false,
-				trailingslashit( basename( $this->plugin_dir ) ) . 'lib/wp-update-migrate/languages'
-			);
+			$i10n_path = trailingslashit( basename( $this->package_dir ) ) . 'lib/wp-update-migrate/languages';
+
+			if ( 'plugin' === $this->package_type ) {
+				load_plugin_textdomain( 'wp-update-migrate', false, $i10n_path );
+			} elseif ( 'theme' === $this->package_type ) {
+				load_theme_textdomain( 'wp-update-migrate', $i10n_path );
+			}
+
 			wp_cache_add_non_persistent_groups( 'wp-update-migrate' );
 		}
 
@@ -165,7 +207,7 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 			$result = true;
 
 			if ( ! $this->update_file_exists_for_version( $this->to_version ) ) {
-				$result = $this->update_plugin_version( $this->to_version );
+				$result = $this->update_package_version( $this->to_version );
 			}
 
 			if ( true !== $result ) {
@@ -200,7 +242,7 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 		}
 
 		protected function build_update_path() {
-			$file_list   = glob( $this->plugin_dir . 'updates' . DIRECTORY_SEPARATOR . '*.php' );
+			$file_list   = glob( $this->package_dir . 'updates' . DIRECTORY_SEPARATOR . '*.php' );
 			$update_path = array();
 
 			foreach ( $file_list as $file_path ) {
@@ -222,7 +264,7 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 
 			require_once $update_file;
 
-			$function_name = $this->plugin_prefix . '_update_to_' . str_replace( '.', '_', $version );
+			$function_name = $this->package_prefix . '_update_to_' . str_replace( '.', '_', $version );
 
 			if ( function_exists( $function_name ) ) {
 				$update_executed = call_user_func( $function_name );
@@ -234,9 +276,10 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 				$error = new WP_Error(
 					__METHOD__,
 					sprintf(
-						// translators: %1$s is the missing function name, %2$s is the path to WP_CONTENT_DIR
-						__( '<br/>The update failed: function <code>%1$s</code> not found.<br/>Please restore the previously used version of the plugin, or delete the plugin and its files in the <code>%2$s</code> directory if any and install the latest version.', 'wp-update-migrate' ),
+						// translators: %1$s is the missing function name, %2$s is the package type, %3$s is the path to WP_CONTENT_DIR
+						__( '<br/>The update failed: function <code>%1$s</code> not found.<br/>Please restore the previously used version of the %2$s, or delete the %2$s and its files in the <code>%2$s</code> directory if any and install the latest version.', 'wp-update-migrate' ),
 						$function_name,
+						$this->package_type,
 						self::get_content_dir()
 					)
 				);
@@ -246,7 +289,7 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 
 				return $this->handle_error( $error );
 			} else {
-				$result = $this->update_plugin_version( $version );
+				$result = $this->update_package_version( $version );
 
 				if ( true === $result ) {
 					// translators: %1$s is the version we just updated to
@@ -263,18 +306,18 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 
 		protected function update_file_exists_for_version( $version ) {
 
-			return file_exists( $this->plugin_dir . 'updates' . DIRECTORY_SEPARATOR . $version . '.php' );
+			return file_exists( $this->package_dir . 'updates' . DIRECTORY_SEPARATOR . $version . '.php' );
 		}
 
 		protected function get_update_file_path_for_version( $version ) {
 
-			return $this->plugin_dir . 'updates' . DIRECTORY_SEPARATOR . $version . '.php';
+			return $this->package_dir . 'updates' . DIRECTORY_SEPARATOR . $version . '.php';
 		}
 
-		protected function update_plugin_version( $version ) {
+		protected function update_package_version( $version ) {
 
 			if ( ! version_compare( $this->from_version, $version, '=' ) ) {
-				$result = update_option( $this->plugin_prefix . '_plugin_version', $version, false );
+				$result = update_option( $this->package_prefix . '_' . $this->package_type . '_version', $version, false );
 			} else {
 				$result = true;
 			}
@@ -282,8 +325,8 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 			if ( ! $result ) {
 				$result = new WP_Error(
 					__METHOD__,
-					// translators: %1$s is the plugin prefix, %2$s is the version number we're trying to update to
-					sprintf( __( 'Failed to update the <code>%1$s_plugin_version</code> to %1$s in the options table.', 'wp-update-migrate' ), $this->plugin_prefix, $version )
+					// translators: %1$s is the package prefix, %2$s is the package type, %3$s is the version number we're trying to update to
+					sprintf( __( 'Failed to update the <code>%1$s_%2$s_version</code> to %3$s in the options table.', 'wp-update-migrate' ), $this->package_prefix, $this->package_type, $version )
 				);
 			}
 
@@ -291,10 +334,10 @@ if ( ! class_exists( 'WP_Update_Migrate' ) ) {
 		}
 
 		protected function handle_error( $error = null ) {
-			// translators: %1$s is the plugin version to update to
-			$error_title = $this->plugin_name . ' - ' . sprintf( __( 'Error updating to version %1$s', 'wp-update-migrate' ), $this->to_version );
-			// translators: %1$s is the path to WP_CONTENT_DIR
-			$error_message = sprintf( __( '<br/>An unexpected error has occured during the update.<br/>Please restore the previously used version of the plugin, or delete the plugin and its files in the <code>%1$s</code> directory if any and install the latest version.', 'wp-update-migrate' ), self::get_content_dir() );
+			// translators: %1$s is the package version to update to
+			$error_title = $this->package_name . ' - ' . sprintf( __( 'Error updating to version %1$s', 'wp-update-migrate' ), $this->to_version );
+			// translators: %1$s is the path to WP_CONTENT_DIR, %2$s is the package type
+			$error_message = sprintf( __( '<br/>An unexpected error has occured during the update.<br/>Please restore the previously used version of the %2$s, or delete the %2$s and its files in the <code>%1$s</code> directory if any and install the latest version.', 'wp-update-migrate' ), self::get_content_dir(), $this->package_type );
 
 			if ( $error instanceof WP_Error ) {
 				$error_message = implode( '<br/><br/>', $error->get_error_messages() );
